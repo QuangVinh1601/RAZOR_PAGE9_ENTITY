3640ec21fb661db194a22e249a76625264bcfce8
Quang Vinh Nguyễn Hữu <nguyenhuuquangvinh5@gmail.com>
2024-02-27 21:57:20 +0700

    adjust forgot and reset password page

--- Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
@@ -31,8 +31,9 @@ namespace RAZOR_PAGE9_ENTITY.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required]
-            [EmailAddress]
+            [Required(ErrorMessage ="Phải nhập địa chỉ email")]
+            [EmailAddress(ErrorMessage ="Vui lòng nhập đúng địa chỉ Email")]
+            [Display(Name ="Địa chỉ Email")]
             public string Email { get; set; }
         }
 
@@ -59,8 +60,8 @@ namespace RAZOR_PAGE9_ENTITY.Areas.Identity.Pages.Account
 
                 await _emailSender.SendEmailAsync(
                     Input.Email,
-                    "Reset Password",
-                    $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    "Đặt lại mật khẩu",
+                    $"Vui lòng <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>bấm vào đây</a>để đặt lại mật khẩu.");
 
                 return RedirectToPage("./ForgotPasswordConfirmation");
             }
